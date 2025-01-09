a4ad35d7d1d47fb883a4bb260fb84550619fd393
mayong <mayong@wudi360.com>
2025-01-09 14:17:37 +0800

    #F2F2F2

--- dsbridge/DWKWebView.m
+++ dsbridge/DWKWebView.m
@@ -200,18 +200,18 @@ commitPreviewingViewController:(UIViewController *)previewingViewController
     }
 }
 
-- (void)webView:(WKWebView *)webView
-requestMediaCapturePermissionForOrigin:(WKSecurityOrigin *)origin
-initiatedByFrame:(WKFrameInfo *)frame
-           type:(WKMediaCaptureType)type
-decisionHandler:(void (^)(WKPermissionDecision))decisionHandler  API_AVAILABLE(ios(15.0))
-{
-    if(self.DSUIDelegate && [self.DSUIDelegate respondsToSelector:@selector(webView:requestMediaCapturePermissionForOrigin:initiatedByFrame:type:decisionHandler:)]) {
-        [self.DSUIDelegate webView:webView requestMediaCapturePermissionForOrigin:origin initiatedByFrame:frame type:type decisionHandler:decisionHandler];
-    } else {
-        decisionHandler(WKPermissionDecisionGrant);
-    }
-}
+//- (void)webView:(WKWebView *)webView
+//requestMediaCapturePermissionForOrigin:(WKSecurityOrigin *)origin
+//initiatedByFrame:(WKFrameInfo *)frame
+//           type:(WKMediaCaptureType)type
+//decisionHandler:(void (^)(WKPermissionDecision))decisionHandler  API_AVAILABLE(ios(15.0))
+//{
+//    if(self.DSUIDelegate && [self.DSUIDelegate respondsToSelector:@selector(webView:requestMediaCapturePermissionForOrigin:initiatedByFrame:type:decisionHandler:)]) {
+//        [self.DSUIDelegate webView:webView requestMediaCapturePermissionForOrigin:origin initiatedByFrame:frame type:type decisionHandler:decisionHandler];
+//    } else {
+//        decisionHandler(WKPermissionDecisionGrant);
+//    }
+//}
 
 - (void) evalJavascript:(int) delay
 {
